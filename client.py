28373eef6713d2b0ef0406c3a352f3ab721b1f31
karthikzzzzzzz <karthikrajan025@gmail.com>
2025-03-20 13:17:40 +0530

    Removed agent-frontend from index

--- client.py
+++ client.py
@@ -2,8 +2,17 @@ from fastapi import HTTPException,FastAPI
 from starlette import status
 from schema import Request
 from services import chat
+from fastapi.middleware.cors import CORSMiddleware
 
 app= FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.post("/chat")
 async def chat_with_ai(request: Request):
